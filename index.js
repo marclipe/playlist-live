cf3fb17166ab8a5849f2b429c316d332a53e925a
Marcos Felipe S.L <marcos.lima52@fatec.sp.gov.br>
2022-11-13 16:13:55 -0300

    Adicionando meu EJS no projeto

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
 require("dotenv").config();
 const express = require("express");
-const connectToDb = require("./database/db");
+const connetToDb = require("./database/db");
+const path = require("path");
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-connectToDb();
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded());
 
-app.get("/hello", (req, res) => {
-  res.send("Hello World!!");
+connetToDb();
+
+app.get("/", (req, res) => {
+  res.render("index");
 });
+
+app.get("/admin", (req, res) => {
+  res.render("admin")
+})
 
 app.listen(port, () =>
   console.log(`Servidor rodando em http:///localhost:${port}`)
